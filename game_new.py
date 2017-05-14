4c79d9487dfac218e0a4b6ba463ac092a7382884
Mayank Bhushan <miku.bhushan@gmail.com>
2017-05-15 04:33:40 +0530

    Fixed minor bugs

--- game_new.py
+++ game_new.py
@@ -3,34 +3,37 @@ import random
 
 class Game:
 	def __init__(self):
-		self.buttons = 2500
 		self.createButtons()
 
 	def createButtons(self):
 		self.buttons = {}
 		for i in range(0,400):
 			status = random.choice([1,0])
-			self.buttons[i] = [Button(root,bg='yellow' if status == 1 else 'black'),status]
+			self.buttons[i] = [Button(root,bg=('yellow' if status == 1 else 'black')),status]
 			self.buttons[i][0].grid(row = i//20,column = i%20)
 
 	def run(self):
-		self.buttons_new = {}
 		for k in range(0,400):
-			status = self.buttons[k][1]
 			row = k//20
 			column = k%20
 			c = 0
 			for i in [row-1,row,row+1]:
 				for j in [column-1,column,column+1]:
 					if self.validButton(i,j):
-						c = c + self.buttons[i*j][1]
-			if c <=1 or c>=4 :
-				self.buttons_new[k] = [self.buttons[k][0],0]
+						c = c + self.buttons[i*20+j][1]
+
+			print (c)
+			c = c - self.buttons[k][1]
+			if (c <=1 or c>=4) :
 				self.buttons[k][0].configure(bg='black')
-			else:
-				self.buttons_new[k] = [self.buttons[k][0],1]
+			elif self.buttons[k][1]==1 or c==3:
 				self.buttons[k][0].configure(bg='yellow')
-		self.buttons = self.buttons_new
+			else:
+				self.buttons[k][0].configure(bg='black')
+		for k in range(0,400):
+			self.buttons[k][1] = 0 if self.buttons[k][0].cget('bg') == 'black' else 1
+			if(k%20 == 0):
+				print("  ")
 		root.after(500,self.run)
 
 	def validButton(self,i,j):
